370676b9bb0f90b8a76a2c8bebac4fb7b55bbffc
Xenaloth <>
2022-03-28 18:00:37 -0500

    Add YouTube support

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
-import tiktok,instagram,json,os
+import tiktok,instagram,youtube,json,os
 creds = open('creds.json',)
 creds = json.load(creds)
 def start(update: Update, context: CallbackContext) -> None:
@@ -20,11 +20,22 @@ def tiktokHandler(update: Update, context: CallbackContext):
         os.remove("./"+filename)
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="An error occured processing the TikTok.")
+def youtubeHandler(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Processing...")
+    #try:
+    filename = youtube.downloadVideo(update.message.text)
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open(filename, 'rb'))
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Here's your YouTube video!")
+    os.remove("./"+filename)
+    #except:
+    #    context.bot.send_message(chat_id=update.effective_chat.id, text="An error occured processing the YouTube video.")
 updater = Updater(creds["telegram_token"])
 updater.dispatcher.add_handler(CommandHandler('start', start))
 instagram_handler = MessageHandler(Filters.regex('(instagram\.com)'), instagramHandler)
 tiktok_handler = MessageHandler(Filters.regex('(tiktok\.com)'), tiktokHandler)
+youtube_handler = MessageHandler(Filters.regex('(youtube\.com|youtu\.be)'), youtubeHandler)
 updater.dispatcher.add_handler(instagram_handler)
 updater.dispatcher.add_handler(tiktok_handler)
+updater.dispatcher.add_handler(youtube_handler)
 updater.start_polling()
 updater.idle()
