c90c0003a96f30a08f4379207c2d5f6e79f94154
Xenaloth <>
2022-04-01 02:55:47 -0500

    disable playlist downloading for youtube

--- youtube.py
+++ youtube.py
@@ -3,5 +3,5 @@ num = 0
 
 def downloadVideo(url):
     num = random.randrange(10000,99999)
-    os.system("./yt-dlp --sponsorblock-mark all --sub-format best --sub-langs en.* --embed-subs  --embed-thumbnail -f 'bv+ba/b' -o "+str(num)+" --merge-output-format mkv "+url)
+    os.system("./yt-dlp --no-playlist --sponsorblock-mark all --sub-format best --sub-langs en.* --embed-subs  --embed-thumbnail -f 'bv+ba/b' -o "+str(num)+" --merge-output-format mkv "+url)
     return str(num)+'.mkv'
